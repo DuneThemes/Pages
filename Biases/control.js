a5187f4068d19824c931310e5ddb3acbad160d9c
DuneThemes <stroud1996@gmail.com>
2015-07-04 16:47:23 +0100

    Create control.js

---
+++ Biases/control.js
@@ -0,0 +1,46 @@
+// JavaScrpt file designed for use with Biases One Page, from Dune Themes
+// dunethemes.tumblr.com
+
+$(document).ready(function(){
+	$('.one').mouseenter(function(){
+		$(this).children('.image').animate({left: '-200px'});	
+	})
+	$('.one').mouseleave(function(){
+		$(this).children('.image').animate({left: '0px'});	
+	})
+	
+	$('.two').mouseenter(function(){
+		$(this).children('.image').animate({left: '-200px'});	
+	})
+	$('.two').mouseleave(function(){
+		$(this).children('.image').animate({left: '0px'});	
+	})
+	
+	$('.three').mouseenter(function(){
+		$(this).children('.image').animate({left: '-200px'});	
+	})
+	$('.three').mouseleave(function(){
+		$(this).children('.image').animate({left: '0px'});	
+	})
+	
+	$('.four').mouseenter(function(){
+		$(this).children('.image').animate({left: '-200px'});	
+	})
+	$('.four').mouseleave(function(){
+		$(this).children('.image').animate({left: '0px'});	
+	})
+	
+	$('.five').mouseenter(function(){
+		$(this).children('.image').animate({left: '-200px'});	
+	})
+	$('.five').mouseleave(function(){
+		$(this).children('.image').animate({left: '0px'});	
+	})
+	
+	$('.six').mouseenter(function(){
+		$(this).children('.image').animate({left: '-200px'});	
+	})
+	$('.six').mouseleave(function(){
+		$(this).children('.image').animate({left: '0px'});	
+	})
+});
